1faa19fe9be07cd439c3065be4962a1ff493f276
Mariana Campos <marianacristinadecampos@gmail.com>
2021-10-25 12:25:50 -0300

    entendo o que sao os arrays

--- introducaoArray.js
+++ introducaoArray.js
@@ -1,8 +1,16 @@
 // === INTRODUCAO AO ARRAY === //
 
-// const media1 = 8.7;
-// const media2 = 7.5;
-// const media3 = 8.9;
-// const media4 = 10;
+/* p r o b l e m a
 
-// const mediaJhin = 
+const media1 = 8.7;
+const media2 = 7.5;
+const media3 = 8.9;
+const media4 = 10;
+
+const mediaLuke = Math.round((media1 + media2 + media3 + media4) / 4);
+console.log(mediaJhin);
+
+const mediaLeia = ?? */
+
+const leia = [10, 9.8, 9.6, 10];
+const luke = [7.6, 8.9, 8.3, 7];
