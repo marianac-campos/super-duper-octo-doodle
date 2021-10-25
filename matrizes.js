27fa4f9148210f0044ab1ccc2c22282ad2af0330
Mariana Campos <marianacristinadecampos@gmail.com>
2021-10-25 12:35:57 -0300

    matrizes/arraybidimensional

---
+++ matrizes.js
@@ -0,0 +1,11 @@
+const frutasPortugues = ['Abacaxi', 'Morango', 'Uva', 'Melancia'];
+const frutasIngles = ['Pineapple', 'Strawberry', 'Grape', 'Watermelon'];
+const frutasFrances = ['Ananas', 'Fraise', 'Raisin', 'Pastèque'];
+
+const tradutor = [frutasPortugues, frutasIngles, frutasFrances];
+
+console.log(` ======== ${tradutor[0][1]} ========`);
+console.log(`INGLES: ${tradutor[1][1]} | FRANCES: ${tradutor[2][2]}`);
+
+console.log(`\n ======== ${tradutor[0][3]} ========`);
+console.log(`INGLES: ${tradutor[1][3]} | FRANCES: ${tradutor[2][3]}`);
