f81753b6805a5f7a1d2ca6962670c0fefcfebe64
Mariana Campos <marianacristinadecampos@gmail.com>
2021-10-26 07:53:13 -0300

    entendendo os callback

---
+++ callbacks.js
@@ -0,0 +1,15 @@
+// ==== C A L L B A C K S ==== //
+
+const jogosFav = [
+    'The Legends of Zelda - Ocarina of Time',
+    'The Legends of Zelda - Majoras Mask',
+    'League of Legends',
+    'The Last of Us',
+    'Uncharted'
+];
+
+jogosFav.forEach(ImprimeNomes);
+
+function ImprimeNomes(nome){
+    console.log(nome);
+}
